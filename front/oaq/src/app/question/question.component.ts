a92c9a0b722bba0b8f05dbba4a28a3a29cf28175
IJCO8830 <jeremy.cotigny@pole-emploi.fr>
2019-08-05 15:48:47 +0200

    Feat ajout du commit question

--- front/oaq/src/app/question/question.component.ts
+++ front/oaq/src/app/question/question.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {SERVER_EVENTS} from "../constant";
 import {Socket} from "ngx-socket-io";
-import {Questions} from "../player/player.model";
+import {Question, Questions} from "../player/player.model";
 
 @Component({
   selector: 'app-question',
@@ -14,13 +14,32 @@ export class QuestionComponent implements OnInit {
   constructor(private readonly socket: Socket) { }
 
   questions: Questions;
+  question: Question;
+  index: number;
 
   ngOnInit() {
-      console.log("Questions RUN");
-      this.socket.on(SERVER_EVENTS.QUESTIONS, (questionsServeur: Questions) => {
-          this.questions = questionsServeur;
-          console.log("Questions serveur : " + JSON.stringify(this.questions));
+      this.index = 0;
+      this.socket.on(SERVER_EVENTS.QUESTIONS, (questionsServeur: any) => {
+          this.questions = questionsServeur.questions;
+          if(this.questions) {
+              this.questionSuivante();
+          }
+      });
+      this.socket.on(SERVER_EVENTS.NEXT_QUESTIONS, () => {
+          console.log("NEXT QUESTIONS");
+          this.questionSuivante();
       });
   }
 
+  questionSuivante() {
+      if(this.questions) {
+          console.log("Open question - index : " + this.index.toString());
+          this.question = this.questions[this.index];
+          console.log("question en cour: " + JSON.stringify(this.questions[this.index]));
+          if (this.question) {
+              this.index ++;
+              console.log("Close question - index : " + this.index.toString());
+          }
+      }
+  }
 }
